86e90c8d4905fa9bc13dee9bb86824dc0bae5551
appyens <an.anurag@msn.com>
2020-06-20 16:37:05 +0530

    hacker earth problems added

---
+++ programs/patterns/main.py
@@ -0,0 +1,39 @@
+for i in range(4):
+
+    for j in range(i + 1):
+        print(' * ', end=' ')
+    print('*')
+
+
+# Python 3.x code to demonstrate star pattern
+
+# Function to demonstrate printing pattern triangle
+def triangle(n):
+    # number of spaces
+    k = 2 * n - 2
+
+    # outer loop to handle number of rows
+    for i in range(0, n):
+
+        # inner loop to handle number spaces
+        # values changing acc. to requirement
+        for j in range(0, k):
+            print(end=" ")
+
+            # decrementing k after each loop
+        k = k - 1
+
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, 7):
+            # printing stars
+            print(j, end="")
+
+            # ending line after each row
+        print("\r")
+
+    # Driver Code
+
+
+n = 4
+triangle(n)
