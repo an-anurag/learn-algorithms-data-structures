9780a52668978aeb5ef67a66d9e2d86061429b5f
appyens <an.anurag@msn.com>
2020-08-20 22:48:40 +0530

    programs added

--- implementations/algorithms/searching/binary.py
+++ implementations/algorithms/searching/binary.py
@@ -1,24 +1,50 @@
-def BinarySearch(lys, val):
-    first = 0
-    last = len(lys)-1
-    index = -1
-    while (first <= last) and (index == -1):
-        mid = (first+last)//2
-        if lys[mid] == val:
-            index = mid
+"""
+Implement binary serach
+"""
+
+
+# iterative version
+def binary_search(arr, ele):
+
+    start = 0
+    end = len(arr) - 1
+    found = False
+
+    while start <= end and not found:
+        mid = (start + end) // 2
+        if arr[mid] == ele:
+            found = True
         else:
-            if val < lys[mid]:
-                last = mid -1
+            if ele < arr[mid]:
+                end = mid - 1
             else:
-                first = mid +1
-    return index
+                start = mid + 1
+
+    return found
 
 
-def linear_search(arr, element):
-    for i in range(len(arr)):
-        if arr[i] == element:
+# res = binary_search([1, 2, 3, 4, 5, 6, 78, 85, 101, 123], 102)
+# print(res)
+
+
+# recursive version
+
+def binary_search_recur(arr, ele):
+
+    # base case to mark where to stop
+    if len(arr) == 0:
+        return False
+
+    else:
+        mid = len(arr) // 2
+
+        if arr[mid] == ele:
             return True
-        return -1
+
+        # recursive calls
+        if ele < arr[mid]:
+            return binary_search_recur(arr[: mid], ele)
+        return binary_search_recur(arr[mid + 1:], ele)
 
 
-print(BinarySearch(sorted([2, 7, 89, 34, 9, 78, 23, 12]), 23))
+print(binary_search_recur([1, 2, 3, 4, 5, 6, 78, 85, 101, 123], 8))
